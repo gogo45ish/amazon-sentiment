01dee97a357991fe4f912f192de5fd138c0a5b29
gogo45ish <43780348+gogo45ish@users.noreply.github.com>
2021-05-05 19:39:29 +0300

    Update server.js

--- server.js
+++ server.js
@@ -406,6 +406,17 @@ app.get('/api/chart3', cors(), asyncHandler(async (req, res) => {
 
 }));
 
+if (process.env.NODE_ENV === 'production') {
+    // Exprees will serve up production assets
+    app.use(express.static('client/build'));
+
+    // Express serve up index.html file if it doesn't recognize route
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
 const port = 5000;
 
 app.listen(process.env.PORT || port, () => `Server running on port ${port}`);
